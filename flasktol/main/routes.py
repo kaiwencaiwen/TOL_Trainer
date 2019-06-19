16fa5f57d74c65699718a5828d9c8c82343db3cc
kaiwencaiwen <kevin.chen2b2020@stu.tes.tp.edu.tw>
2019-06-19 19:49:43 +0800

    test commit

--- flasktol/main/routes.py
+++ flasktol/main/routes.py
@@ -9,16 +9,16 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    # user = User(username="kevinchen", email="kevin.chen2b2020@stu.tes.tp.edu.tw", password="$2b$12$Us0QRgN4HSLKwzlfjXSpsuPbQUd4UIbrzd8rHr5RksovTsSVUplz6", access=3)
-    # db.session.add(user)
-    # db.session.commit()
-    return render_template("home.html")
+	user = User.query.filter_by(username="kchen").first_or_404()
+	user.access=3
+	db.session.commit()
+	return render_template("home.html")
 
 # questions page
 
 
 @main.route("/questions")
 def questions():
-    page = request.args.get('page', 1, type=int)
-    questions = Question.query.order_by(Question.date_posted.desc()).paginate(page=page, per_page=10)
-    return render_template("questions.html", title="Questions", questions=questions)
+	page = request.args.get('page', 1, type=int)
+	questions = Question.query.order_by(Question.date_posted.desc()).paginate(page=page, per_page=10)
+	return render_template("questions.html", title="Questions", questions=questions)
